0387646484a72dd7b6c8f4b7b6748665882415ab
George Aliev <gera08012329@gmail.com>
2023-08-05 19:20:39 +0300

    feat: add typescript

--- server/controllers/todo.controllers.ts
+++ server/controllers/todo.controllers.ts
@@ -1,21 +1,22 @@
 const db = require('../db')
+import { Request, Response } from "express"
 
 class TodoController {
-  async addTodo(req, res) {
-    const { id, content, done } = req.body
-    const newTodo = await db.query('INSERT INTO todo (id, content, done) values ($1, $2, $3) RETURNING *', [id, content, done])
+  async addTodo(req: Request, res: Response) {
+    const { content, done } = req.body
+    const newTodo = await db.query('INSERT INTO todo (content, done) values ($1, $2) RETURNING *', [content, done])
     res.json(newTodo.rows[0])
   }
-  async removeTodo(req, res) {
+  async removeTodo(req: Request, res: Response) {
     const id = req.params.id
     const todo = await db.query('DELETE FROM todo where id = $1', [id])
     res.json(todo.rows[0])
   }
-  async getAllTodos(req, res) {
+  async getAllTodos(req: Request, res: Response) {
     const todos = await db.query('SELECT * FROM todo')
     res.json(todos.rows)
   }
-  async updateTodo(req, res) {
+  async updateTodo(req: Request, res: Response) {
     const { id, content, done } = req.body
     const updatedTodo = await db.query('UPDATE todo set content = $1, done = $2 where id = $3 RETURNING *', [content, done, id])
     res.json(updatedTodo.rows[0])
